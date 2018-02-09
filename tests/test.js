e6279dd40e13583f4ceba1715252c11955da0ae9
Tomáš Korenko <radixxko@gmail.com>
2018-02-09 08:09:16 +0100

    Tests

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,8 @@
 const fs = require('fs');
 const Parser = require('../lib/parser');
 
-const parser = new Parser({ quotes: '\'"', tokens: '.,;:(){}[]-+<>%/*=' });
+const parser = new Parser({ quotes: '\'"', tokens: '.,;:(){}[]-+<>%/*=@?|&' }, fs.readFileSync( __dirname + '/templates/javascript.syntax', 'utf8'));
 
-console.log( parser.parse( fs.readFileSync( __dirname + '/templates/javascript.template', 'utf8') ) );
+let template = parser.parse( fs.readFileSync( __dirname + '/templates/javascript.template', 'utf8') );
+
+console.log({ template, syntax: parser.syntax });
